bd71af7efa61e0aea84fee0a96961a7dfeea0fd8
Andrew Martin <a.a.d.martin@gmail.com>
2015-01-29 08:34:39 +1100

    fixed case error in test

--- client/client_test.go
+++ client/client_test.go
@@ -8,7 +8,7 @@ import  (
 
 func TestGenUuid(t *testing.T) {
 
-        var validUUID = regexp.MustCompile(`[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}`)
+        var validUUID = regexp.MustCompile(`[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}`)
 
         uuid := genUuid()
 
