09486b4328a9c534eca057e9a271f06402cd48fe
Andrew Martin <a.a.d.martin@gmail.com>
2015-02-17 08:45:46 +1100

    Added more simple tests

--- client/client_test.go
+++ client/client_test.go
@@ -1,23 +1,28 @@
-
 package main
 
-import  (
-        "testing"
-        "regexp"
-        )
+import (
+	"regexp"
+	"testing"
+)
 
 func TestGenUuid(t *testing.T) {
 
-        var validUUID = regexp.MustCompile(`[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}`)
+	var validUUID = regexp.MustCompile(`[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}`)
 
-        uuid := genUuid()
+	uuid := genUuid()
 
-        if validUUID.MatchString(uuid) == false {
-             t.Error("UUID Gen Fail")
-        }
+	if validUUID.MatchString(uuid) == false {
+		t.Error("UUID Gen Fail")
+	}
 
 }
 
+func TestMd5String(t *testing.T) {
 
+	expectedOutput := `d67c5cbf5b01c9f91932e3b8def5e5f8`
 
+	if md5String("teststring") != expectedOutput {
+		t.Error("MD5 String failed")
+	}
 
+}
